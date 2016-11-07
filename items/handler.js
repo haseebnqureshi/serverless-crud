1857955711a7483adea9c726513a9380887795c4
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-11-07 12:51:32 -0500

    basic crud service with lambda helpers and item modeling

--- items/handler.js
+++ items/handler.js
@@ -1,37 +1,73 @@
 'use strict';
 
+var lib = require('lib');
+
+var Helpers = {
+
+	respond: (context, err, result) => {
+		var statusCode = 200;
+		var body = result;
+
+		if (!result) {
+			statusCode = 400;
+			result = [{}];
+		}
+
+		if (result.length == 0) {
+			statusCode = 404;
+		}
+
+		if (err) {
+			statusCode = 500;
+			body = { err: err };
+		}
+
+		var response = {
+			statusCode: statusCode,
+			body: JSON.stringify(body)
+		};
+
+		return context.succeed(response);
+	}
+
+};
+
+module.exports.create = (event, context, callback) => {
+
+	lib.items.create(event, (err, result) => {
+
+		return Helpers.respond(context, err, result);
+	});
+};
+
+module.exports.readAll = (event, context, callback) => {
+
+	lib.items.readAll(event, (err, result) => {
+
+		return Helpers.respond(context, err, result);
+	});
+};
+
 module.exports.read = (event, context, callback) => {
-	let user = {
-		id: 123,
-		name: 'New User',
-		email: 'new@user.com'
-	};
-	const response = {
-		statusCode: 200,
-		headers: {},
-		body: JSON.stringify(user)
-	};
-	context.succeed(response);
+
+	lib.items.read(event, (err, result) => {
+
+		return Helpers.respond(context, err, result);
+	});
 };
 
 module.exports.update = (event, context, callback) => {
-	let user = {
-		id: 123,
-		name: 'New User',
-		email: 'new@user.com'
-	};
-	const response = {
-		statusCode: 200,
-		headers: {},
-		body: JSON.stringify(user)
-	};
-	context.succeed(response);
+
+	lib.items.update(event, (err, result) => {
+
+		return Helpers.respond(context, err, result);
+	});
 };
 
 module.exports.delete = (event, context, callback) => {
-	const response = {
-		statusCode: 204,
-		headers: {}
-	};
-	context.succeed(response);
+
+	lib.items.delete(event, (err, result) => {
+
+		return Helpers.respond(context, err, result);
+	});
 };
