904288e99fa9ca69c6d70dbe4d0b2e7f96f05054
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-11-08 13:04:17 -0500

    added helpers for api event handlers, cleaner syntax for crud routing

--- items/handler.js
+++ items/handler.js
@@ -1,76 +1,33 @@
 'use strict';
 
-var lib = require('lib');
-
-var Helpers = {
-
-	respond: (context, err, result, statusCodeOverride) => {
-		var statusCode = 200;
-		var body = result;
-
-		if (result.length === 0) {
-			statusCode = 400;
-		}
-
-		if (err) {
-			statusCode = 500;
-			body = { err: err };
-		}
-
-		if (statusCodeOverride) {
-			statusCode = statusCodeOverride;
-		}
-
-		var response = {
-			statusCode: statusCode,
-			body: JSON.stringify(body)
-		};
-
-		if (statusCode === 204) {
-			response = { statusCode: response.statusCode };
-		}
-
-		return context.succeed(response);
-	}
-
-};
+var Lib = require('lib');
 
 module.exports.create = (event, context, callback) => {
-
-	lib.items.create(event, (err, result) => {
-
-		return Helpers.respond(context, err, result);
+	Lib.Items.Create(event, (err, result) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
 	});
 };
 
 module.exports.readAll = (event, context, callback) => {
-
-	lib.items.readAll(event, (err, result) => {
-
-		return Helpers.respond(context, err, result);
+	Lib.Items.ReadAll(event, (err, result) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
 	});
 };
 
 module.exports.read = (event, context, callback) => {
-
-	lib.items.read(event, (err, result) => {
-
-		return Helpers.respond(context, err, result);
+	Lib.Items.Read(event, (err, result) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
 	});
 };
 
 module.exports.update = (event, context, callback) => {
-
-	lib.items.update(event, (err, result) => {
-
-		return Helpers.respond(context, err, result);
+	Lib.Items.Update(event, (err, result) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
 	});
 };
 
 module.exports.delete = (event, context, callback) => {
-
-	lib.items.delete(event, (err, result) => {
-
-		return Helpers.respond(context, err, result, 204);
+	Lib.Items.Delete(event, (err, result) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
 	});
 };
