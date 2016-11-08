ed614678882e976ede203fff216865ebfa01a023
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-11-08 13:36:57 -0500

    introduced descriptive messages from data persistence layer on transaction

--- items/handler.js
+++ items/handler.js
@@ -3,31 +3,31 @@
 var Lib = require('lib');
 
 module.exports.create = (event, context, callback) => {
-	Lib.Items.Create(event, (err, result) => {
-		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
+	Lib.Items.Create(event, (err, result, message) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context, message);
 	});
 };
 
 module.exports.readAll = (event, context, callback) => {
-	Lib.Items.ReadAll(event, (err, result) => {
-		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
+	Lib.Items.ReadAll(event, (err, result, message) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context, message);
 	});
 };
 
 module.exports.read = (event, context, callback) => {
-	Lib.Items.Read(event, (err, result) => {
-		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
+	Lib.Items.Read(event, (err, result, message) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context, message);
 	});
 };
 
 module.exports.update = (event, context, callback) => {
-	Lib.Items.Update(event, (err, result) => {
-		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
+	Lib.Items.Update(event, (err, result, message) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context, message);
 	});
 };
 
 module.exports.delete = (event, context, callback) => {
-	Lib.Items.Delete(event, (err, result) => {
-		return Lib.ApiHelpers.easyRespond(result, err, 200, context);
+	Lib.Items.Delete(event, (err, result, message) => {
+		return Lib.ApiHelpers.easyRespond(result, err, 200, context, message);
 	});
 };
